2e7372e48741c4cff5003dba3b27f7a6e4ced680
skander bardaoui <skonbardaoui@gmail.com>
2026-01-18 21:01:39 +0100

    first commit

---
+++ src/Controller/CommercialController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CommercialController extends AbstractController
+{
+    #[Route('/commercial', name: 'app_commercial')]
+    public function index(): Response
+    {
+        return $this->render('commercial/index.html.twig', [
+            'controller_name' => 'CommercialController',
+        ]);
+    }
+}
